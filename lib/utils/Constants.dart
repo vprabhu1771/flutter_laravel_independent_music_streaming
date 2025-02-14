286edaf35b4ed1aef94949ddf7b9b7b56ee9ddd7
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-14 17:40:49 +0530

    Create Constants.dart

---
+++ lib/utils/Constants.dart
@@ -0,0 +1,25 @@
+class Constants{
+
+  static const String SERVER_DOMAIN= "http://192.168.1.122:8000";
+
+  static const String BASE_URL = SERVER_DOMAIN + "/api";
+
+  static const String GENRE_ROUTE ="/genres";
+
+  static const String BRAND_ROUTE="/brands";
+
+  static const String SONG_ROUTE="/songs";
+
+  static const String SONG_FILTER_BY_GENRE_ROUTE="/songs?genre_id=";
+
+
+  static const String SONG_FILTER_BY_BRAND_ROUTE="/songs?brand_id=";
+
+  static const String LOGIN_ROUTE = "/login";
+
+  static const String LOGOUT_ROUTE ="/logout";
+
+  static const String USER_REGISTER_ROUTE ="/register";
+
+  static const String USER_INFO_ROUTE ="/user";
+}
